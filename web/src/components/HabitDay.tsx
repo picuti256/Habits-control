8fd6fabd67635f53239a678a7dc9a785c9cbe920
picuti256 <picuti256@gmail.com>
2023-01-23 15:12:40 -0300

    Criação dos componentes Headers, habits e pop up do habito

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,7 +1,25 @@
+import * as Popover from "@radix-ui/react-popover";
+
 // interface HabitsProps {
 //     completed: number
 // }
 
 export function HabitDay() {
-	return <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>;
+	return (
+		<Popover.Root>
+			<Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+
+			<Popover.Portal>
+				<Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+					<span className="font-semibold text-zinc-400">Segunda-feira</span>
+					<span className="mt-1 font-extrabold leading-tight text-3xl">23/01</span>
+
+					<div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+						<div className="h-3 rounded-xl bg-violet-600 w-3/4" />
+					</div>
+					<Popover.Arrow height={8} width={16} className="fill-zinc-800" />
+				</Popover.Content>
+			</Popover.Portal>
+		</Popover.Root>
+	)
 }
---
+++ web/src/components/NewHabitForm.tsx
@@ -0,0 +1,20 @@
+import { Check } from "phosphor-react";
+
+export function NewHabitForm() {
+	return (
+		<form className="w-full flex flex-col mt-6">
+			<label htmlFor="title" className="font-semibold leading-tight">
+				Qual o seu compromentimento?
+			</label>
+
+			<input type="text" id="title" className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-300" placeholder="Ex: exercitar-se, dormir bem, etc..." autoFocus />
+
+			<label className="font-semibold leading-tight mt-4">Qual a recorrência?</label>
+
+			<button type="submit" className="mt-6 rounded-lg p-4 flex items-center gap-3 font-semibold bg-green-600 justify-center hover:bg-green-500">
+				<Check size={20} weight="bold" />
+				Confirmar
+			</button>
+		</form>
+	);
+}
